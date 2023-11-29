1681872028420aa9353c7e3d6e71dc949081f0ba
EdenRize <edenrize@gmail.com>
2023-11-29 10:55:20 +0200

    add imges and some style

---
+++ js/services/meme-service.js
@@ -0,0 +1,43 @@
+var gImgs = [
+  { id: 1, url: './img/2.jpg', keywords: ['happy', 'women'] },
+  { id: 2, url: './img/003.jpg', keywords: ['funny', 'men', 'mad'] },
+  { id: 3, url: './img/004.jpg', keywords: ['cute', 'animal'] },
+  { id: 4, url: './img/5.jpg', keywords: ['funny', 'cute', 'baby'] },
+  { id: 5, url: './img/005.jpg', keywords: ['cute', 'animal', 'baby'] },
+  { id: 6, url: './img/006.jpg', keywords: ['cute', 'animal'] },
+  { id: 7, url: './img/8.jpg', keywords: ['funny', 'men', 'smile'] },
+  { id: 8, url: './img/9.jpg', keywords: ['funny', 'baby', 'smile'] },
+  { id: 9, url: './img/12.jpg', keywords: ['men'] },
+  { id: 10, url: './img/19.jpg', keywords: ['men', 'mad'] },
+  { id: 11, url: './img/Ancient-Aliens.jpg', keywords: ['funny', 'men'] },
+  { id: 12, url: './img/drevil.jpg', keywords: ['funny', 'men'] },
+  { id: 13, url: './img/img2.jpg', keywords: ['funny', 'baby', 'smile'] },
+  { id: 14, url: './img/img4.jpg', keywords: ['men', 'mad'] },
+  {
+    id: 15,
+    url: './img/img5.jpg',
+    keywords: ['funny', 'cute', 'baby', 'smile', 'happy'],
+  },
+  { id: 16, url: './img/img6.jpg', keywords: ['funny', 'cute', 'animal'] },
+  { id: 17, url: './img/img11.jpg', keywords: ['men', 'funny', 'smile'] },
+  { id: 18, url: './img/img12.jpg', keywords: ['funny', 'men'] },
+  { id: 19, url: './img/leo.jpg', keywords: ['men', 'smile'] },
+  { id: 20, url: './img/meme1.jpg', keywords: ['men', 'mad'] },
+  { id: 21, url: './img/One-Does-Not-Simply.jpg', keywords: ['men', 'mad'] },
+  { id: 22, url: './img/Oprah-You-Get-A.jpg', keywords: ['women', 'happy'] },
+  { id: 23, url: './img/patrick.jpg', keywords: ['men', 'funny', 'smile'] },
+  { id: 24, url: './img/putin.jpg', keywords: ['men'] },
+  { id: 25, url: './img/X-Everywhere.jpg', keywords: ['men'] },
+]
+var gMeme = {
+  selectedImgId: 5,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: 'I sometimes eat Falafel',
+      size: 20,
+      color: 'red',
+    },
+  ],
+}
+var gKeywordSearchCountMap = { funny: 12, animal: 16, baby: 2 }

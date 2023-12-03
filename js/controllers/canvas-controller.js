255b20b3ddb2d1435c9c552dcf9f4c708e225342
EdenRize <edenrize@gmail.com>
2023-12-03 08:38:14 +0200

    remove focus for txt input

--- js/controllers/canvas-controller.js
+++ js/controllers/canvas-controller.js
@@ -120,7 +120,6 @@ function onMove(ev) {
 function onUp(ev) {
   gDraggedLine = null
   document.querySelector('canvas').style.cursor = 'grab'
-  document.querySelector('.meme-text-input').focus()
 }
 
 function getClickedLine(pos) {
